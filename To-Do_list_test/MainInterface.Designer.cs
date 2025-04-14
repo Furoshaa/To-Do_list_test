100bdfd1a32d6a8991186a24a8f4401d69776a2b
Furoshaa <levent.kyllian@gmail.com>
2025-04-14 19:36:56 +0200

    Add a function to the button to take the text from the textBox and add it to the listBox

--- To-Do_list_test/MainInterface.Designer.cs
+++ To-Do_list_test/MainInterface.Designer.cs
@@ -23,51 +23,52 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            listBox1 = new ListBox();
-            textBoxNewTask = new TextBox();
-            button1 = new Button();
+            listBox1 = new System.Windows.Forms.ListBox();
+            textBoxNewTask = new System.Windows.Forms.TextBox();
+            buttonAdd = new System.Windows.Forms.Button();
             SuspendLayout();
             // 
             // listBox1
             // 
             listBox1.FormattingEnabled = true;
             listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(12, 12);
+            listBox1.Location = new System.Drawing.Point(12, 12);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(776, 304);
+            listBox1.Size = new System.Drawing.Size(631, 289);
             listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
             // 
             // textBoxNewTask
             // 
-            textBoxNewTask.Location = new Point(12, 336);
+            textBoxNewTask.Location = new System.Drawing.Point(12, 336);
             textBoxNewTask.Name = "textBoxNewTask";
-            textBoxNewTask.Size = new Size(311, 23);
+            textBoxNewTask.Size = new System.Drawing.Size(311, 23);
             textBoxNewTask.TabIndex = 1;
             textBoxNewTask.TextChanged += textBoxNewTask_TextChanged;
             // 
-            // button1
+            // buttonAdd
             // 
-            button1.Location = new Point(329, 336);
-            button1.Name = "button1";
-            button1.Size = new Size(87, 23);
-            button1.TabIndex = 2;
-            button1.Text = "Add new task";
-            button1.UseVisualStyleBackColor = true;
+            buttonAdd.Location = new System.Drawing.Point(329, 336);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Size = new System.Drawing.Size(87, 23);
+            buttonAdd.TabIndex = 2;
+            buttonAdd.Text = "Add new task";
+            buttonAdd.UseVisualStyleBackColor = true;
+            buttonAdd.Click += buttonAdd_Click;
             // 
             // MainInterface
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(655, 450);
+            Controls.Add(buttonAdd);
             Controls.Add(textBoxNewTask);
             Controls.Add(listBox1);
-            Name = "MainInterface";
             Text = "To-do List";
             Load += MainInterface_Load;
             ResumeLayout(false);
@@ -76,8 +77,8 @@
 
         #endregion
 
-        private ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox1;
         private TextBox textBoxNewTask;
-        private Button button1;
+        private Button buttonAdd;
     }
 }
--- To-Do_list_test/MainInterface.cs
+++ To-Do_list_test/MainInterface.cs
@@ -16,5 +16,27 @@ namespace To_Do_list_test
         {
 
         }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+
+            string newTask = textBoxNewTask.Text.Trim();
+
+            if (!string.IsNullOrWhiteSpace(newTask))
+            {
+                listBox1.Items.Add(newTask);
+                textBoxNewTask.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Please enter a task", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
